b126dcd483b3f92950b917a70a6b36c18ab13f18
JBlanc <49165786+Yvea@users.noreply.github.com>
2019-10-18 16:04:18 +0200

    Update Unit1.cpp

--- TP3 Four Code/Unit1.cpp
+++ TP3 Four Code/Unit1.cpp
@@ -14,6 +14,7 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
 	maCarte = new(PCI9111);
+	ID_Card = -1;
 }
 //---------------------------------------------------------------------------
 
@@ -22,28 +23,32 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 
 void __fastcall TForm1::btnInitialisationCarteClick(TObject *Sender)
 {
-	int success = maCarte->Initialisation();
-
-	if (success == 1)
+	ID_Card = maCarte->Initialisation();
+	
+	if(ID_Card == -1)
 	{
-        Label1->Color = clGreen;
+		Label1->Color = clRed;
 	}
+	else
+	{
+		Label1->Color = clGreen;
+	}
+	
 }
 //---------------------------------------------------------------------------
 
 
 void __fastcall TForm1::Button1Click(TObject *Sender)
 {
-  float Tension_lu = maCarte->Acquisition();
-  Edit1->Text=Tension_lu;
+ 	float Tension_lu = maCarte->Acquisition();
+ 	Edit1->Text=Tension_lu;
 }
 //---------------------------------------------------------------------------
 
 
 void __fastcall TForm1::EcritureClick(TObject *Sender)
 {
-	float tension = (Edit2->Text).ToDouble();
-    maCarte->Ecriture(tension);
+	maCarte->Ecriture(Edit2->Text.ToDouble());
 }
 //---------------------------------------------------------------------------
 
