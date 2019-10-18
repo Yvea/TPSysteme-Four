14c48c163693a84ac3b1573e1104f5dcca947a81
JBlanc <49165786+Yvea@users.noreply.github.com>
2019-10-18 15:59:37 +0200

    Update PCI9111.cpp

--- TP3 Four Code/PCI9111.cpp
+++ TP3 Four Code/PCI9111.cpp
@@ -20,28 +20,22 @@ int PCI9111::Initialisation()
 {
 	ID_Card = Register_Card(PCI_9111DG, 0);
 
-	AI_9111_Config(ID_Card, TRIG_INT_PACER, P9111_TRGMOD_SOFT, 0);
-	AO_9111_Config(ID_Card, P9111_AO_UNIPOLAR);
+	AI_9111_Config(this->ID_Card, TRIG_INT_PACER, P9111_TRGMOD_SOFT, 0);
+	AO_9111_Config(this->ID_Card, P9111_AO_UNIPOLAR);
 
-	if (ID_Card >= 0)
-	{
-		return 1;
-	}
-	else
-	{
-        return 0;
+        return this->ID_Card;
     }
 
 }
 float PCI9111::Acquisition()
 {
   	F64 Tension_lu;
-	AI_VReadChannel(ID_Card,1, AD_B_10_V, &Tension_lu);
-    return Tension_lu;
+	AI_VReadChannel(this->ID_Card,1, AD_B_10_V, &Tension_lu);
+    	return Tension_lu;
 
 }
 
 void PCI9111::Ecriture(float tension)
 {
-    AO_VWriteChannel(ID_Card, 1, tension);
+    AO_VWriteChannel(this->ID_Card, 1, tension);
 }
